6584112d98088e8c66ead6a9fc0178c12488f8c6
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-06-03 04:49:26 +0100

    implement mutation

--- server/server.go
+++ server/server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Faroukhamadi/connectify-native/ent"
 	"github.com/Faroukhamadi/connectify-native/graph"
 	"github.com/Faroukhamadi/connectify-native/graph/generated"
+	_ "github.com/lib/pq"
+
 	"github.com/joho/godotenv"
 )
 
@@ -21,9 +26,18 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	dbUrl := os.Getenv("DATABASE_URL")
+	// dbUrl := os.Getenv("DATABASE_URL") + "?sslmode=disable"
+	dbUrl := "postgresql://postgres:16%2F04%2F2002farouk@localhost:5432/Connectify?sslmode=disable"
 
-	log.Print(dbUrl)
+	client, err := ent.Open("postgres", dbUrl)
+	fmt.Println("database url: ", dbUrl)
+
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+	ctx := context.Background()
+	example(ctx, client)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -37,4 +51,14 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func example(ctx context.Context, client *ent.Client) {
+	farouk := client.User.
+		Create().
+		SetFirstName("Farouk").
+		SetLastName("Hamadi").
+		SetUsername("farakrakk").
+		SetPassword("1234").SaveX(ctx)
+	fmt.Println("User created: ", farouk)
 }
